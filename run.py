6c3779a89fe2ca5ecc74ca26f4f50b01f9eb9f79
carlos <carlos.val@bifi.es>
2014-11-13 17:53:26 +0100

    Start appplication

---
+++ run.py
@@ -0,0 +1,9 @@
+from app.core import create_app
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host=app.config['HOST'], port=app.config['PORT'],
+        debug=app.config.get('DEBUG', True))
+
+else:
+    app = create_app()
