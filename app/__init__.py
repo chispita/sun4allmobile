93e4ac4f4ed54d5f86e4e8c928fc91614bd18301
carlos <carlos.val@bifi.es>
2014-11-13 17:51:16 +0100

    Remove app.run

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,1 @@
-from flask import Flask
-
-app = Flask(__name__)
-from app import views
-
-
-#if __name__ == "__main__":  # pragma: no cover
-#    #logging.basicConfig(level=logging.NOTSET)
-#    app.run( debug=True)
-
-    #app.run(host=app.config['HOST'], port=get_port(),
-    #                    debug=app.config.get('DEBUG', True))
 
